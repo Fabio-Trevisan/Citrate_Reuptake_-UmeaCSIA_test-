22f182172b7a929da623e7b2337d01e5d92d0f2f
Fabio-Trevisan <98732667+Fabio-Trevisan@users.noreply.github.com>
2023-08-24 16:17:34 +0200

    Isotopologues histogram trial (still to be completed)

--- InHouse_Graphs.R
+++ InHouse_Graphs.R
@@ -8,7 +8,8 @@ library(rstatix)
 library(purrr)
 library(agricolae)
 library(reshape2)
-
+library(ggbreak)
+library(ggh4x)
 
 #TO DO
 #Histogram for each molecule and time to show isotopologues enrichment 
@@ -100,12 +101,12 @@ f3 <- ggplot(Summary_table_L_Significant, aes(x = Time, y = mean, group = Treatm
   theme_bw() + 
   scale_y_continuous(labels = percent) +
   scale_x_continuous(breaks=seq(0,120,15))
-f4 <- f3 + facet_wrap(~metabolite, scales="fixed") + 
+f4 <- f3 + facet_wrap(~metabolite, scales="free") + 
   ylab("% Enrichment") + 
   xlab("Time (min)") 
 f4
 
-ggsave(filename = paste("InHouse_", Class, "_Scatterplot_Significant.pdf", sep=""), plot = f4, dpi = 600, units = "cm", width = 80, height = 60, scale = 0.35)
+ggsave(filename = paste("InHouse_", Class, "_Scatterplot_Significant_2.pdf", sep=""), plot = f4, dpi = 600, units = "cm", width = 80, height = 60, scale = 0.35)
 
 
 
@@ -162,3 +163,87 @@ f6 <- f5 + facet_wrap(~metabolite, scales="fixed")+
 f6
 
 ggsave(filename = paste("InHouse_", Class, "_BoxPlot_Significant.pdf", sep=""), plot = f6, dpi = 600, units = "cm", width = 80, height = 60, scale = 0.35)
+
+# Histogram ISOTOPOLOGUES ####
+##   Dataset preparation ####
+#cleaning
+df <- table[,-c(3,5:7,9:10)] #remove un-useful columns
+df <- df[!str_detect(df$sample, "blank"),] #remove blank samples
+df <- df[!str_detect(df$sample, "std"),] #remove standards
+df <- df[!str_detect(df$sample, "QC"),] #remove QCs
+row.names(df) <- NULL
+
+#Time-Treatment-Labeling 
+Treatment_factors <- read.csv(paste("Treatment_factors_", Class, "_isotopologues.csv", sep=""), sep=";", header=T) #load file with treatment_factors
+df$Treatment <- Treatment_factors$Treatment
+df$Time <- Treatment_factors$Time
+df$Labeling <- Treatment_factors$Labeling #add them to df
+
+#Uniformation and vectors creation
+df$metabolite <- factor(df$metabolite)
+df$Treatment <- factor(df$Treatment)
+df$Time <- factor(df$Time)
+df$Labeling <- factor(df$Labeling) #convert character and numerbers to factors
+
+vector_metabolite <- levels(factor(df$metabolite))
+vector_Treatment <- levels(factor(df$Treatment))
+vector_Time <- levels(factor(df$Time))
+vector_Labeling <- levels(factor(df$Labeling)) #write vectors for subseting
+
+#Replace 0 with random values of 1/10 +/- 10% of min value
+df["isotopologue_fraction"][df["isotopologue_fraction"]==0] <- NA #raplace meanenrichment 0 values with 1/10 ot 1/100 of min values
+x <- min(df$isotopologue_fraction, na.rm = T)/10 #calculate min values/10
+df[is.na(df)] <-  runif(sum(is.na(df)), min = x-(x/10), max = x+(x/10))
+
+
+
+##   Summary table ####
+Summary_table <- ddply(df, c("metabolite", "Time", "Labeling", "Treatment", "isotopologue"), summarise,
+                       N    = sum(!is.na(isotopologue_fraction)),
+                       mean = mean(isotopologue_fraction, na.rm=TRUE),
+                       sd   = sd(isotopologue_fraction, na.rm=TRUE),
+                       se   = sd / sqrt(N))
+
+Summary_table_L <- Summary_table %>% filter(str_detect(Labeling, "L"))
+
+
+
+##    Graph ####
+OA_Significant_Molecules <- "Aconitate|Citrate|Hydroxyglutarate|Isocitrate|Oxoglutarate"
+#Pyruvate, Succinare, Oxalate and Oxaloacetate P<0.05 but removed because non-sense 
+
+AA_Significant_Molecules <- "Aspartate|Citrulline|GABA|Glutamate|Glutamine"
+#Lysine and Alanine 0.05<P<0.10 
+
+if (Class == "AA") {
+  Summary_table_L_Significant <- Summary_table_L %>% 
+    filter(str_detect(metabolite, gsub('["]', '', AA_Significant_Molecules)))
+} else { 
+  Summary_table_L_Significant <- Summary_table_L %>% 
+    filter(str_detect(metabolite, gsub('["]', '', OA_Significant_Molecules)))
+}
+
+f7 <- ggplot(Summary_table_L_Significant, aes(x = isotopologue, y = mean, fill = Time)) + 
+  geom_bar(stat = "identity", position = 'dodge', width = 0.8) + 
+  geom_errorbar(aes(ymin = mean-se, ymax = mean+se, group = Time), width = 0.5, position = position_dodge(0.8)) +
+  scale_fill_manual(values=c("grey77", "skyblue3", "blue", "navy")) +
+  theme_bw() + 
+  scale_x_continuous(breaks=seq(0,6,1)) +
+  scale_y_continuous(labels = percent)
+
+f8 <- f7 + facet_wrap(~metabolite+Treatment, scales="fixed", ncol = 3) +
+  ylab("% Enrichment") + 
+  xlab("Isotopologue") 
+f8 
+
+#Breaks NOT WORKING
+scale_y_break(c(0.10, 0.85))
+scale_y_cut(breaks = 0.1)
+scale_y_continuous(labels = percent, limits = c(NA, 0.10), oob = scales::oob_keep) +
+scale_y_continuous(labels = percent, limits = c(0.85, NA), oob = scales::oob_keep)
+
+
+
+ggsave(filename = paste("InHouse_", Class, "_Barplot_Isotopologues.pdf", sep=""), plot = f8, dpi = 600, units = "cm", width = 100, height = 200, scale = 0.3)
+
+
